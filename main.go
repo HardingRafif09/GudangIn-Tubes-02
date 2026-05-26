0dda4422c57b0e046bc3574420ce5dbf0992c25d
Harding Rafif <hardingrdp3@gmail.com>
2026-05-26 13:30:06 +0700

    update main.go

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func tambahBarang(A *DaftarBarang, n *int) {
 	fmt.Print("Stok Barang : ")
 	fmt.Scan(&A[*n].Stok)
 
-	*n++
+	*n = *n + 1
 
 	fmt.Println("Barang berhasil ditambahkan")
 }
@@ -101,7 +101,7 @@ func hapusBarang(A *DaftarBarang, n *int) {
 			A[i] = A[i+1]
 		}
 
-		*n--
+		*n = *n - 1
 
 		fmt.Println("Barang berhasil dihapus")
 	} else {
@@ -117,7 +117,7 @@ func main() {
 	for {
 		fmt.Println("===== MENU =====")
 		fmt.Println("1. Tambah Barang")
-		fmt.Println("2. Tampil Barang")
+		fmt.Println("2. Display Barang")
 		fmt.Println("3. Ubah Barang")
 		fmt.Println("4. Hapus Barang")
 		fmt.Println("5. Keluar")
@@ -125,12 +125,16 @@ func main() {
 		fmt.Scan(&pilihan)
 
 		if pilihan == 1 {
+			fmt.Println()
 			tambahBarang(&data, &n)
 		} else if pilihan == 2 {
+			fmt.Println()
 			tampilBarang(data, n)
 		} else if pilihan == 3 {
+			fmt.Println()
 			ubahBarang(&data, n)
 		} else if pilihan == 4 {
+			fmt.Println()
 			hapusBarang(&data, &n)
 		} else if pilihan == 5 {
 			fmt.Println("Program selesai")
@@ -140,5 +144,79 @@ func main() {
 		}
 
 		fmt.Println()
+	}
+}
+
+func catatTransaksi(gudang *DaftarBarang, jmlBarang int, trx *DaftarTransaksi, jmlTrx *int ) {
+	var jenis, kode string 
+	var jumlah, indeks int
+	var ketemu bool = false 
+
+	fmt.Println("\n== CATAT TRANSAKSI BARANG ==")
+
+	if jmlBarang == 0 { 
+		fmt.Println("Gudang kosong! Tambahkan master barang terlebih dahulu.")
+		return
+	}
+
+	fmt.Print("Masukkan Kode Barang: ") 
+	fmt.Scan(&kode)
+
+	for i := 0; i < jmlBarang; i++ {
+		if gudang[i].Kode == kode {
+			ketemu = true 
+			indeks = i
+			break
+		}
+	}
+
+	if !ketemu {
+		fmt.Println("Transaksi Gagal Barang Tidak terdaftar")
+		return
+	}
+
+	fmt.Printf("Barang: %s | Stok saat ini: %d\n", gudang[indeks].Nama, gudang[indeks].Stok)
+	fmt.Print("Jenis Transaksi ('Masuk' / 'Keluar'): ")
+	fmt.Scan(&jenis)
+	fmt.Print("Jumlah Barang: ")
+	fmt.Scan(&jumlah)
+
+	if jenis == "Masuk" { 
+		gudang[indeks].Stok += jumlah
+		fmt.Printf("Sukses: Stok %s bertambah menjadi %d\n", gudang[indeks].Nama, gudang[indeks].Stok)
+	} else if jenis == "Keluar" { 
+		if gudang[indeks].Stok >= jumlah {
+			gudang[indeks].Stok -= jumlah
+			fmt.Printf("Sukses: Stok %s berkurang menjadi %d\n", gudang[indeks].Nama, gudang[indeks].Stok)
+		} else {
+			fmt.Println("Transaksi Gagal Stok di gudang tidak mencukupi!")
+			return
+		}
+	} else {
+		fmt.Println("Transaksi Gagal: Jenis transaksi tidak dikenal.")
+		return
+	}
+
+	trx[*jmlTrx].Kode = kode
+	trx[*jmlTrx].Jenis = jenis
+	trx[*jmlTrx].Jumlah = jumlah
+	*jmlTrx = *jmlTrx + 1
+}
+
+func tampilkanRiwayat(trx DaftarTransaksi, jmlTrx int) {
+	fmt.Println("\n== Riwatat Transaksi Gudang ===")
+
+	if jmlTrx == 0 {
+		fmt.Println("Belum ada transaksi yang tercatat")
+		return
+	}
+
+	for i := 0; i < jmlTrx; i++ {
+
+		fmt.Printf("%d. Barang: %s | Jenis: %s | Jumlah: %d pcs\n",
+			i+1,
+			trx[i].Kode,
+			trx[i].Jenis,
+			trx[i].Jumlah)
 	}
 }
